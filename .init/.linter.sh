57f108773127bb7f3645aeebe3578a5ccfeb07c8
Kavia AI <production@kavia.ai>
2025-12-05 14:48:05 +0000

    Checkpoint 7 - cm824172e1

--- .init/.linter.sh
+++ .init/.linter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/kavia/workspace/code-generation/kanban-task-manager-121675/kanban_board_frontend
+cd /home/kavia/workspace/code-generation/kanban-task-manager-121675-287149
 npm run build
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
